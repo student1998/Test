e8759922b8d743d4f40d685494daf4649fced1f0
stdent1998 <student1998@gmail.com>
2010-10-12 17:03:54 +0200

    0.11

---
+++ src/de/fhhof/model/Product.java
@@ -0,0 +1,16 @@
+package de.fhhof.model;
+
+public class Product {
+	
+	String name;
+	String serNum;
+	String model;
+	String category;
+	long id;
+	
+	
+	public Product(){
+		
+	}
+
+}
---
+++ src/de/fhhof/view/MWindow.java
@@ -0,0 +1,5 @@
+package de.fhhof.view;
+
+public class MWindow {
+
+}
